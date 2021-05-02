5e69792d2fc9c53c2797229abd959d520f5394b9
Diogo Cabral <diogocs1@gmail.com>
2021-05-02 00:06:51 -0300

    Atualizando comando para executar direto no docker

--- cmd.prod.sh
+++ cmd.prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # docker-compose exec myzapprod ../../../bin/bash
-docker run --name myzap -p 3333:3333 -e ENGINE=VENOM diogocs1/myzap
+docker run --name myzap -p 3333:3333 -e ENGINE=VENOM -d diogocs1/myzap
